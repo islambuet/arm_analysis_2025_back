44e644ac31df172c4fe3425e39b9593006f495b8
Shaiful islam <islambuet@gmail.com>
2024-12-01 18:33:36 +0600

    tasks reduced

--- config/variables/tables/main.php
+++ config/variables/tables/main.php
@@ -6,7 +6,6 @@ $db_main=env('DB_DATABASE','rnd_2023');
 define('TABLE_USERS', $db_main.'.users');
 define('TABLE_PRINCIPALS', $db_main.'.principals');
 define('TABLE_COMPETITORS', $db_main.'.competitors');
-define('TABLE_DESIGNATIONS', $db_main.'.designations');
 
 define('TABLE_CROPS', $db_main.'.crops');
 define('TABLE_CROP_TYPES', $db_main.'.crop_types');
