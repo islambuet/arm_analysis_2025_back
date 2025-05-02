f248950b5b19e047dae4a898182d4d79926b2bf4
Shaiful Islam <shaiful.islam@aclusterllc.com>
2025-05-02 23:31:27 +0600

    distributor added

--- app/Http/Controllers/setup/route.api.php
+++ app/Http/Controllers/setup/route.api.php
@@ -109,6 +109,15 @@ Route::middleware('logged-user')->group(function()use ($url,$controllerClass){
     Route::match(['GET','POST'],$url.'/get-item/{itemId}', [$controllerClass, 'getItem']);
     Route::post($url.'/save-item', [$controllerClass, 'saveItem']);
 });
+$url='setup/distributor';
+$controllerClass= Controllers\setup\DistributorController::class;
+/** @noinspection DuplicatedCode */
+Route::middleware('logged-user')->group(function()use ($url,$controllerClass){
+    Route::match(['GET','POST'],$url.'/initialize', [$controllerClass, 'initialize']);
+    Route::match(['GET','POST'],$url.'/get-items', [$controllerClass, 'getItems']);
+    Route::match(['GET','POST'],$url.'/get-item/{itemId}', [$controllerClass, 'getItem']);
+    Route::post($url.'/save-item', [$controllerClass, 'saveItem']);
+});
 
 $url='setup/location_divisions';
 $controllerClass= Controllers\setup\LocationDivisionsController::class;
--- config/variables/tables/main.php
+++ config/variables/tables/main.php
@@ -16,6 +16,7 @@ define('TABLE_PACK_SIZES', $db_main.'.pack_sizes');
 define('TABLE_LOCATION_PARTS', $db_main.'.location_parts');
 define('TABLE_LOCATION_AREAS', $db_main.'.location_areas');
 define('TABLE_LOCATION_TERRITORIES', $db_main.'.location_territories');
+define('TABLE_DISTRIBUTORS', $db_main.'.distributors');
 define('TABLE_LOCATION_DIVISIONS', $db_main.'.location_divisions');
 define('TABLE_LOCATION_DISTRICTS', $db_main.'.location_districts');
 define('TABLE_LOCATION_UPAZILAS', $db_main.'.location_upazilas');
